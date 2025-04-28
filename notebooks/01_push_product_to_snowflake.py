770053a1151327c917503055c2d43ce384747f0d
Nirupam Kumar Velagapudi <67449392+vnks18@users.noreply.github.com>
2025-04-28 17:09:15 -0500

    Update 01_push_product_to_snowflake.py

--- notebooks/01_push_product_to_snowflake.py
+++ notebooks/01_push_product_to_snowflake.py
@@ -4,7 +4,7 @@ from pyspark.sql.functions import col, when, lower
 
 df = spark.read.csv("dbfs:/mnt/data/project2/data/product_master.csv", header=True, inferSchema=True)
 
-# Merge of main + dev logic
+# Merge logic
 df_transformed = df.dropDuplicates(["ProductID"]).withColumn("Price", when(col("Price") < 0, 0).otherwise(col("Price"))).withColumn("Category", lower(col("Category"))).withColumn("IsPremium", when(col("Price") > 1000, "Yes").otherwise("No")).withColumn("PriceBucket", when(col("Price") < 200, "Low")
                               .when(col("Price") < 700, "Medium").otherwise("High"))
 
