86858d3c5841a7a8b07620311cb9ee605b8ffa64
Nirupam Kumar Velagapudi <67449392+vnks18@users.noreply.github.com>
2025-04-28 17:06:12 -0500

    Update 01_push_product_to_snowflake.py

--- notebooks/01_push_product_to_snowflake.py
+++ notebooks/01_push_product_to_snowflake.py
@@ -1,9 +1,12 @@
-# PySpark script to push data to Snowflake via external stage
+# product_master to Snowflake (with enhancements)
+
+from pyspark.sql.functions import col, when, lower
 
 df = spark.read.csv("dbfs:/mnt/data/project2/data/product_master.csv", header=True, inferSchema=True)
 
-# Clean-up & transformation
-df_clean = df.dropDuplicates(["ProductID"])     .withColumn("Price", when(col("Price") < 0, 0).otherwise(col("Price")))
+# Merge of main + dev logic
+df_transformed = df.dropDuplicates(["ProductID"]).withColumn("Price", when(col("Price") < 0, 0).otherwise(col("Price"))).withColumn("Category", lower(col("Category"))).withColumn("IsPremium", when(col("Price") > 1000, "Yes").otherwise("No")).withColumn("PriceBucket", when(col("Price") < 200, "Low")
+                              .when(col("Price") < 700, "Medium").otherwise("High"))
 
 # Write as Parquet to external stage
-df_clean.write     .mode("overwrite")     .parquet("dbfs:/mnt/staged-data/product_master/")  # Synced to Snowflake external stage
+df_transformed.write.mode("overwrite").parquet("dbfs:/mnt/staged-data/product_master/")
